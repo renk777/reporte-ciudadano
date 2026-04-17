e74670567ea6779cfadb0a0b96b1a8e3d14a881e
kenner varilla <varillakenner@gmail.com>
2026-04-16 20:54:46 -0500

    pool de conexiones

--- db.js
+++ db.js
@@ -1,21 +1,24 @@
 require('dotenv').config();
 const mysql = require('mysql2');
- 
-const db = mysql.createConnection({
+
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
- 
-db.connect((err) => {
+
+pool.getConnection((err, connection) => {
   if (err) {
     console.error('Error de conexión:', err);
   } else {
     console.log('Conectado a MySQL remoto');
+    connection.release();
   }
 });
- 
-module.exports = db;
- 
+
+module.exports = pool;
