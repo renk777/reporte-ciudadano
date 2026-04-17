fd6ab560ef43699aa5c0dcfb1bb893a7ccf6b8d4
kenner varilla <varillakenner@gmail.com>
2026-04-16 20:58:44 -0500

    servir html desde servidor

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 
 const reportRoutes = require('./reportRoutes');
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
-
 app.use('/uploads', express.static('uploads'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', reportRoutes);
 
-app.listen(3000, () => {
-  console.log('Servidor escuchando en http://localhost:3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Servidor escuchando');
 });
